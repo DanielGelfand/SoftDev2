6d9fedfd48f0b3f0cf6ccafc341910d0369c70c7
DanielGelfand <dgelfand@stuy.edu>
2019-02-03 21:47:14 -0500

    semicolons

--- 02_canvas/dotconn.js
+++ 02_canvas/dotconn.js
@@ -5,28 +5,28 @@ K#02 -- Connecting the Dots
 2019-02-04 M
 */
 
-var canvas = document.getElementById('playground')
-var ctx = canvas.getContext('2d')
+var canvas = document.getElementById('playground');
+var ctx = canvas.getContext('2d');
 
-var clear = document.getElementById('clear')
+var clear = document.getElementById('clear');
 
 
-var prev_Pos = []
+var prev_Pos = [];
 
 var dot = function make_dot(x, y) {
-    ctx.fillStyle = "#abcdef"
+    ctx.fillStyle = "#abcdef";
     if (prev_Pos == []) { return }
-    console.log("here")
-    ctx.beginPath()
-    ctx.moveTo(x, y)
-    ctx.lineTo(prev_Pos[0], prev_Pos[1])
-    ctx.stroke()
-    ctx.beginPath()
-    ctx.ellipse(x, y, 5, 5, 360, 0, 360)
-    ctx.ellipse(prev_Pos[0], prev_Pos[1], 5, 5, 360, 0, 360)
-    console.log("there")
-    prev_Pos = [x, y]
-    ctx.fill()
+    console.log("here");
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(prev_Pos[0], prev_Pos[1]);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.ellipse(x, y, 5, 5, 360, 0, 360);
+    ctx.ellipse(prev_Pos[0], prev_Pos[1], 5, 5, 360, 0, 360);
+    console.log("there");
+    prev_Pos = [x, y];
+    ctx.fill();
 }
 
 var cls = function c() {
@@ -34,17 +34,17 @@ var cls = function c() {
     ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight)
     prev_Pos = []
     return true*/
-    location.reload() //refresh page
+    location.reload(); //refresh page
 }
 
 canvas.addEventListener('click', function(e) {
-    dot(e.offsetX, e.offsetY)
+    dot(e.offsetX, e.offsetY);
 })
 
 clear.addEventListener('click', cls )
 
 canvas.oncontextmenu = function(e) {
-    e.preventDefault()
-    prev_Pos = []
-    dot(e.offsetX, e.offsetY)
+    e.preventDefault();
+    prev_Pos = [];
+    dot(e.offsetX, e.offsetY);
 }
