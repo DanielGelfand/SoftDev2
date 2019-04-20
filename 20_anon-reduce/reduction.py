7d9b339ae15133dcec9506e4d681b6efcbc57aa8
DanielGelfand <dgelfand@stuy.edu>
2019-04-20 10:33:59 -0400

    updates

--- 20_anon-reduce/reduction.py
+++ 20_anon-reduce/reduction.py
@@ -1,16 +1,38 @@
+'''
+NotSoBad - Daniel Gelfand and Damian Wasilewicz
+SoftDev2 pd6
+K#20: Reductio ad Absurdum
+2019-04-29
+'''
+
 from functools import reduce
 
-def Process(file):
+def get_words(file):
     f = open(file,'r')
-    words = [''.join(c for c in word if c.isalpha() or c=="'") for word in f.read().split()] 
+    words = [''.join(c for c in word if c.isalpha() or c=="'") for word in f.read().split()]
     f.close()
     return words
 
-print( Process('text.txt') )
+#print( get_words('text.txt') )
 
 def freqWord(file,word):
    # counter = 0
-    return reduce( (lambda w,counter: w if w == word) , Process(file) )
+   words = get_words(file)
+   return reduce(lambda x,y: x+1  if word.lower() == y.lower() else x,words,0)
+
+#print (freqWord('text.txt',"body")) #24
 
 
-print (freqWord('text.txt',"I"))
+def mostCommonWord(file):
+    words = get_words(file)
+    wordCounts = {}
+    for word in words:
+        if word not in wordCounts.keys():
+            wordCounts[word] = freqWord(file,word)
+        #print(wordCounts)
+    return max(wordCounts,key=wordCounts.get)
+
+#print(mostCommonWord('text.txt')) #The
+print (freqWord('text.txt',"The")) #1148
+
+
